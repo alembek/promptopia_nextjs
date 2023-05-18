5acb3ef75b2b8bc597ccd67be8077572e30c8fa4
Alembek Assaubayev <alem.assaubayev@gmail.com>
2023-05-18 17:57:10 +0900

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,9 +8,10 @@ export const metadata={
 }
 
 const RootLayout = ({children}) => {
-  return (
+return (
     <html lang='en'>
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient'/>
             </div>
@@ -19,6 +20,7 @@ const RootLayout = ({children}) => {
                 <Nav/>
                 {children}
             </main>
+            </Provider>
         </body>
     </html>
 )
